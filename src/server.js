6a1e49e02a012c7edd01701479e2bc21efb000d8
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-05-09 04:32:57 +0100

    chore: setup openapi documentation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import log from "fancy-log";
-import cors from "cors";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import config from "./config";
 import DbConnection from "./database";
+import swaggerDoc from "./swagger.json";
 
 const app = express();
 const isProduction = process.env.NODE_ENV === "production";
@@ -17,6 +19,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
